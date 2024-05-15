64dc1ac12dd2d3e1d3e4b66586f86a75ba8a385e
bolognapietro <pietro.bologna@studenti.unitn.it>
2024-05-15 09:46:43 +0200

    update

--- exam/248715_pietro_bologna/NLU/part_2/functions.py
+++ exam/248715_pietro_bologna/NLU/part_2/functions.py
@@ -3,13 +3,10 @@ import os
 import torch
 from conll import evaluate
 from sklearn.metrics import classification_report
-
-
+import torch.nn as nn
 
 device = 'mps' # cuda:0 means we are using the GPU with id 0, if you have multiple GPU
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1" # Used to report errors on CUDA side
-
-
 
 def train_loop(data, optimizer, criterion_slots, criterion_intents, model, clip=5):
     model.train()
@@ -80,3 +77,23 @@ def eval_loop(data, criterion_slots, criterion_intents, model, lang):
     report_intent = classification_report(ref_intents, hyp_intents, 
                                           zero_division=False, output_dict=True)
     return results, report_intent, loss_array
+
+def init_weights(mat):
+    for m in mat.modules():
+        if type(m) in [nn.GRU, nn.LSTM, nn.RNN]:
+            for name, param in m.named_parameters():
+                if 'weight_ih' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.xavier_uniform_(param[idx*mul:(idx+1)*mul])
+                elif 'weight_hh' in name:
+                    for idx in range(4):
+                        mul = param.shape[0]//4
+                        torch.nn.init.orthogonal_(param[idx*mul:(idx+1)*mul])
+                elif 'bias' in name:
+                    param.data.fill_(0)
+        else:
+            if type(m) in [nn.Linear]:
+                torch.nn.init.uniform_(m.weight, -0.01, 0.01)
+                if m.bias != None:
+                    m.bias.data.fill_(0.01)
--- exam/248715_pietro_bologna/NLU/part_2/model.py
+++ exam/248715_pietro_bologna/NLU/part_2/model.py
@@ -1,4 +1,3 @@
-import torch
 import torch.nn as nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
@@ -47,23 +46,99 @@ class ModelIAS(nn.Module):
         # Slot size: batch_size, classes, seq_len
         return slots, intent
     
+class ModelIAS_Bidirectional(nn.Module):
 
-def init_weights(mat):
-    for m in mat.modules():
-        if type(m) in [nn.GRU, nn.LSTM, nn.RNN]:
-            for name, param in m.named_parameters():
-                if 'weight_ih' in name:
-                    for idx in range(4):
-                        mul = param.shape[0]//4
-                        torch.nn.init.xavier_uniform_(param[idx*mul:(idx+1)*mul])
-                elif 'weight_hh' in name:
-                    for idx in range(4):
-                        mul = param.shape[0]//4
-                        torch.nn.init.orthogonal_(param[idx*mul:(idx+1)*mul])
-                elif 'bias' in name:
-                    param.data.fill_(0)
-        else:
-            if type(m) in [nn.Linear]:
-                torch.nn.init.uniform_(m.weight, -0.01, 0.01)
-                if m.bias != None:
-                    m.bias.data.fill_(0.01)
+    def __init__(self, hid_size, out_slot, out_int, emb_size, vocab_len, n_layer=1, pad_index=0):
+        super(ModelIAS_Bidirectional, self).__init__()
+        # hid_size = Hidden size
+        # out_slot = number of slots (output size for slot filling)
+        # out_int = number of intents (output size for intent class)
+        # emb_size = word embedding size
+
+        self.embedding = nn.Embedding(vocab_len, emb_size, padding_idx=pad_index)
+
+        self.utt_encoder = nn.LSTM(emb_size, hid_size, n_layer, bidirectional=True, batch_first=True)
+        
+        # Output size doubled due to bidirectionality
+        self.slot_out = nn.Linear(hid_size*2, out_slot)
+        self.intent_out = nn.Linear(hid_size, out_int)
+        # Dropout layer How/Where do we apply it?
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, utterance, seq_lengths):
+        # utterance.size() = batch_size X seq_len
+        utt_emb = self.embedding(utterance) # utt_emb.size() = batch_size X seq_len X emb_size
+
+        # pack_padded_sequence avoid computation over pad tokens reducing the computational cost
+
+        packed_input = pack_padded_sequence(utt_emb, seq_lengths.cpu().numpy(), batch_first=True)
+        # Process the batch
+        packed_output, (last_hidden, cell) = self.utt_encoder(packed_input)
+
+        # Unpack the sequence
+        utt_encoded, input_sizes = pad_packed_sequence(packed_output, batch_first=True)
+        # Get the last hidden state
+        last_hidden = last_hidden[-1,:,:]
+
+        # Is this another possible way to get the last hiddent state? (Why?)
+        # utt_encoded.permute(1,0,2)[-1]
+
+        # Compute slot logits
+        slots = self.slot_out(utt_encoded)
+        # Compute intent logits
+        intent = self.intent_out(last_hidden)
+
+        # Slot size: batch_size, seq_len, classes
+        slots = slots.permute(0,2,1) # We need this for computing the loss
+        # Slot size: batch_size, classes, seq_len
+        return slots, intent
+
+class ModelIAS_Bidirectional_Drop(nn.Module):
+
+    def __init__(self, hid_size, out_slot, out_int, emb_size, vocab_len, n_layer=1, pad_index=0):
+        super(ModelIAS_Bidirectional, self).__init__()
+        # hid_size = Hidden size
+        # out_slot = number of slots (output size for slot filling)
+        # out_int = number of intents (output size for intent class)
+        # emb_size = word embedding size
+
+        self.embedding = nn.Embedding(vocab_len, emb_size, padding_idx=pad_index)
+
+        self.utt_encoder = nn.LSTM(emb_size, hid_size, n_layer, bidirectional=True, batch_first=True)
+        
+        # Output size doubled due to bidirectionality
+        self.slot_out = nn.Linear(hid_size*2, out_slot)
+        self.intent_out = nn.Linear(hid_size, out_int)
+        # Dropout layer How/Where do we apply it?
+        self.dropout = nn.Dropout(0.1)
+
+    def forward(self, utterance, seq_lengths):
+        # utterance.size() = batch_size X seq_len
+        utt_emb = self.embedding(utterance) # utt_emb.size() = batch_size X seq_len X emb_size
+
+        # pack_padded_sequence avoid computation over pad tokens reducing the computational cost
+
+        packed_input = pack_padded_sequence(utt_emb, seq_lengths.cpu().numpy(), batch_first=True)
+        # Process the batch
+        packed_output, (last_hidden, cell) = self.utt_encoder(packed_input)
+
+        # Unpack the sequence
+        utt_encoded, input_sizes = pad_packed_sequence(packed_output, batch_first=True)
+        # Get the last hidden state
+        last_hidden = last_hidden[-1,:,:]
+
+        # Apply dropout
+        utt_encoded = self.dropout(utt_encoded)
+
+        # Is this another possible way to get the last hiddent state? (Why?)
+        # utt_encoded.permute(1,0,2)[-1]
+
+        # Compute slot logits
+        slots = self.slot_out(utt_encoded)
+        # Compute intent logits
+        intent = self.intent_out(last_hidden)
+
+        # Slot size: batch_size, seq_len, classes
+        slots = slots.permute(0,2,1) # We need this for computing the loss
+        # Slot size: batch_size, classes, seq_len
+        return slots, intent
